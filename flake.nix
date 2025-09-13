2899e619d2a8df7d5377bfea3653a6cb3a74e89a
John Kutz <126092383+Iztuk@users.noreply.github.com>
2025-09-12 19:53:07 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,24 @@
 {
-  description = "NixOS 25.05 + Home Manager + personal dev env + Neovim from GitHub";
+  description = "Home Manager flake for jkutz";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
-    lib = nixpkgs.lib;
+    pkgs = import nixpkgs { inherit system; };
   in {
-    nixosConfigurations.nixos = lib.nixosSystem {
-      inherit system;
-
-      # Make flake inputs available to modules (e.g., home/jkutz.nix)
-      specialArgs = { inherit inputs; };
-
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        ./home/jkutz.nix
-      ];
-    };
+    # HM-only: define a home configuration (no nixosConfigurations here)
+    homeConfigurations."jkutz@nixos" =
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home/jkutz.nix ];
+      };
   };
 }
+
 
